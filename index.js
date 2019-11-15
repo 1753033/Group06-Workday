57bb1c35c5d10e6cad824d244c8bd211af8dded9
1753021 <1753021@student.hcmus.edu.vn>
2019-11-15 11:06:18 +0700

    Hong hieu luon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/Group06'));
+app.use(express.static(__dirname + '/'));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
